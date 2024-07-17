fe6db2dbbe2fbb934e598eec94f09eff19236432
mplogas <m.plogas@berlin.de>
2024-07-17 21:55:08 +0200

    refactored gateway object and mapping. added a list of sensors, removed the hardcoded properties

--- src/Ecowitt.Controller/Mapping/ApiDataExtension.cs
+++ src/Ecowitt.Controller/Mapping/ApiDataExtension.cs
@@ -4,10 +4,9 @@ namespace Ecowitt.Controller.Mapping;
 
 public static class ApiDataExtension
 {
-    public static Gateway Map(this ApiData apiData, string name = "", bool isMetric = true)
+    public static Gateway Map(this ApiData apiData, bool isMetric = true)
     {
-        // TODO: check other apidata properties for freedom units
-        return new Gateway
+        var result = new Gateway()
         {
             PASSKEY = apiData.PASSKEY,
             Model = apiData.Model,
@@ -16,113 +15,133 @@ public static class ApiDataExtension
             Freq = apiData.Freq,
             DateUtc = apiData.DateUtc,
             IpAddress = apiData.IpAddress ?? string.Empty,
-            TimestampUtc = apiData.TimestampUtc,
-            TempIndoor = !isMetric ? apiData.TempInf : F2C(apiData.TempInf),
-            HumidityIndoor = apiData.HumidityIn,
-            BaromRelativeIndoor = apiData.BaromRelIn,
-            BaromAbsoluteIndoor = apiData.BaromAbsIn,
-            Temp = !isMetric ? apiData.TempF : F2C(apiData.TempF),
-            Humidity = apiData.Humidity,
-            WindDir = apiData.WindDir,
-            WindSpeed = !isMetric ? apiData.WindSpeedMph : M2K(apiData.WindSpeedMph),
-            WindGust = !isMetric ? apiData.WindGustMph : M2K(apiData.WindGustMph),
-            MaxDailyGust = !isMetric ? apiData.MaxDailyGust : M2K(apiData.MaxDailyGust),
-            SolarRadiation = apiData.SolarRadiation,
-            Uv = apiData.Uv,
-            RainRate = !isMetric ? apiData.RainRateIn : I2M(apiData.RainRateIn),
-            EventRain = !isMetric ? apiData.EventRainIn : I2M(apiData.EventRainIn),
-            HourlyRain = !isMetric ? apiData.HourlyRainIn : I2M(apiData.HourlyRainIn),
-            DailyRain = !isMetric ? apiData.DailyRainIn : I2M(apiData.DailyRainIn),
-            WeeklyRain = !isMetric ? apiData.WeeklyRainIn : I2M(apiData.WeeklyRainIn),
-            MonthlyRain = !isMetric ? apiData.MonthlyRainIn : I2M(apiData.MonthlyRainIn),
-            YearlyRain = !isMetric ? apiData.YearlyRainIn : I2M(apiData.YearlyRainIn),
-            TotalRain = !isMetric ? apiData.TotalRainIn : I2M(apiData.TotalRainIn),
-            Temp1 = !isMetric ? apiData.Temp1F : F2C(apiData.Temp1F),
-            Humidity1 = apiData.Humidity1,
-            Temp2 = !isMetric ? apiData.Temp2F : F2C(apiData.Temp2F),
-            Humidity2 = apiData.Humidity2,
-            Temp3 = !isMetric ? apiData.Temp3F : F2C(apiData.Temp3F),
-            Humidity3 = apiData.Humidity3,
-            Temp4 = !isMetric ? apiData.Temp4F : F2C(apiData.Temp4F ),
-            Humidity4 = apiData.Humidity4,
-            Temp5 = !isMetric ? apiData.Temp5F : F2C(apiData.Temp5F),
-            Humidity5 = apiData.Humidity5,
-            Temp6 = !isMetric ? apiData.Temp6F : F2C(apiData.Temp6F),
-            Humidity6 = apiData.Humidity6,
-            Temp7 = !isMetric ? apiData.Temp7F : F2C(apiData.Temp7F),
-            Humidity7 = apiData.Humidity7,
-            Temp8 = !isMetric ? apiData.Temp8F : F2C(apiData.Temp8F),
-            Humidity8 = apiData.Humidity8,
-            SoilMoisture1 = apiData.SoilMoisture1,
-            SoilAd1 = apiData.SoilAd1,
-            SoilMoisture2 = apiData.SoilMoisture2,
-            SoilAd2 = apiData.SoilAd2,
-            SoilMoisture3 = apiData.SoilMoisture3,
-            SoilAd3 = apiData.SoilAd3,
-            SoilMoisture4 = apiData.SoilMoisture4,
-            SoilAd4 = apiData.SoilAd4,
-            SoilMoisture5 = apiData.SoilMoisture5,
-            SoilAd5 = apiData.SoilAd5,
-            SoilMoisture6 = apiData.SoilMoisture6,
-            SoilAd6 = apiData.SoilAd6,
-            SoilMoisture7 = apiData.SoilMoisture7,
-            SoilAd7 = apiData.SoilAd7,
-            SoilMoisture8 = apiData.SoilMoisture8,
-            SoilAd8 = apiData.SoilAd8,
-            Pm25Ch1 = apiData.Pm25Ch1,
-            Pm25Avg24hCh1 = apiData.Pm25Avg24hCh1,
-            Pm25Ch2 = apiData.Pm25Ch2,
-            Pm25Avg24hCh2 = apiData.Pm25Avg24hCh2,
-            Pm25Ch3 = apiData.Pm25Ch3,
-            Pm25Avg24hCh3 = apiData.Pm25Avg24hCh3,
-            Pm25Ch4 = apiData.Pm25Ch4,
-            Pm25Avg24hCh4 = apiData.Pm25Avg24hCh4,
-            TfCo2 = apiData.TfCo2,
-            HumiCo2 = apiData.HumiCo2,
-            Pm25Co2 = apiData.Pm25Co2,
-            Pm2524hCo2 = apiData.Pm2524hCo2,
-            Pm10Co2 = apiData.Pm10Co2,
-            Pm1024hCo2 = apiData.Pm1024hCo2,
-            Co2 = apiData.Co2,
-            Co224h = apiData.Co224h,
-            LightningNum = apiData.LightningNum,
-            Lightning = apiData.Lightning,
-            LightningTime = apiData.LightningTime,
-            LeakCh2 = apiData.LeakCh2,
-            TfCh1 = apiData.TfCh1,
-            TfCh2 = apiData.TfCh2,
-            LeafWetnessCh1 = apiData.LeafWetnessCh1,
-            ConsoleBatt = apiData.ConsoleBatt,
-            Wh65batt = apiData.Wh65batt,
-            Wh80batt = apiData.Wh80batt,
-            Wh26batt = apiData.Wh26batt,
-            Batt1 = apiData.Batt1,
-            Batt2 = apiData.Batt2,
-            Batt3 = apiData.Batt3,
-            Batt4 = apiData.Batt4,
-            Batt5 = apiData.Batt5,
-            Batt6 = apiData.Batt6,
-            Batt7 = apiData.Batt7,
-            Batt8 = apiData.Batt8,
-            SoilBatt1 = apiData.SoilBatt1,
-            SoilBatt2 = apiData.SoilBatt2,
-            SoilBatt3 = apiData.SoilBatt3,
-            SoilBatt4 = apiData.SoilBatt4,
-            SoilBatt5 = apiData.SoilBatt5,
-            SoilBatt6 = apiData.SoilBatt6,
-            SoilBatt7 = apiData.SoilBatt7,
-            SoilBatt8 = apiData.SoilBatt8,
-            Pm25Batt1 = apiData.Pm25Batt1,
-            Pm25Batt2 = apiData.Pm25Batt2,
-            Pm25Batt3 = apiData.Pm25Batt3,
-            Pm25Batt4 = apiData.Pm25Batt4,
-            Wh57Batt = apiData.Wh57Batt,
-            LeakBatt2 = apiData.LeakBatt2,
-            TfBatt1 = apiData.TfBatt1,
-            TfBatt2 = apiData.TfBatt2,
-            Co2Batt = apiData.Co2Batt,
-            LeafBatt1 = apiData.LeafBatt1
+            TimestampUtc = apiData.TimestampUtc
         };
+
+        var propertyInfo = typeof(ApiData).GetProperties();
+        foreach (var property in propertyInfo)
+        {
+            if (property.Name.StartsWith("Temp") &&
+                property.Name.EndsWith("f", StringComparison.InvariantCultureIgnoreCase))
+            {
+                var value = (double?)property.GetValue(apiData);
+                var sensor = new Sensor<double?>(property.Name, DeviceClass.Temperature, StateClass.Measurement, isMetric ? "°C" : "F", isMetric ? F2C(value) : value);
+                result.Sensors.Add(sensor);
+            } else if (property.Name.StartsWith("Humi"))
+            {
+                var sensor = new Sensor<int?>(property.Name, DeviceClass.Humidity, StateClass.Measurement, "%", (int?)property.GetValue(apiData));
+                result.Sensors.Add(sensor);
+            } else if (property.Name.StartsWith("Barom"))
+            {
+                var value = (double?)property.GetValue(apiData);
+                var sensor = new Sensor<double?>(property.Name, DeviceClass.Pressure, StateClass.Measurement, isMetric ? "hPa" : "inHg", isMetric? I2M(value) : value);
+                result.Sensors.Add(sensor);
+            } else if (property.Name.StartsWith("WindDir"))
+            {
+                var sensor = new Sensor<int?>(property.Name, DeviceClass.None, StateClass.Measurement, "°", (int?)property.GetValue(apiData));
+                result.Sensors.Add(sensor);
+            } else if ((property.Name.StartsWith("Wind") && property.Name.EndsWith("Mph")) ||
+                       property.Name.StartsWith("MaxDailyGust"))
+            {
+                var value = (double?)property.GetValue(apiData);
+                var sensor = new Sensor<double?>(property.Name, DeviceClass.WindSpeed, StateClass.Measurement,
+                    isMetric ? "km/h" : "mph", isMetric ? M2K(value) : value);
+                result.Sensors.Add(sensor);
+            } else if (property.Name.StartsWith("SolarRadiation"))
+            {
+                var sensor = new Sensor<double?>(property.Name, DeviceClass.Illuminance, StateClass.Measurement, "W/m²",
+                    (double?)property.GetValue(apiData));
+            } else if (property.Name.StartsWith("Uv"))
+            {
+                var sensor = new Sensor<int?>(property.Name, DeviceClass.None, StateClass.Measurement, "UV", (int?)property.GetValue(apiData));
+                result.Sensors.Add(sensor);
+            } else if (property.Name.StartsWith("RainRate") || property.Name.StartsWith("EventRain") ||
+                       property.Name.StartsWith("HourlyRain") || property.Name.StartsWith("DailyRain") ||
+                       property.Name.StartsWith("WeeklyRain") || property.Name.StartsWith("MonthlyRain") ||
+                       property.Name.StartsWith("YearlyRain") || property.Name.StartsWith("TotalRain"))
+            {
+                var value = (double?)property.GetValue(apiData);
+                var stateClass = StateClass.Measurement;
+                if (property.Name.StartsWith("TotalRain"))
+                {
+                    stateClass = StateClass.TotalIncreasing;
+                } else if (property.Name.StartsWith("HourlyRain") || property.Name.StartsWith("DailyRain") ||
+                           property.Name.StartsWith("WeeklyRain") || property.Name.StartsWith("MonthlyRain") ||
+                           property.Name.StartsWith("YearlyRain")) 
+                {
+                    stateClass = StateClass.Total;
+                }
+                
+                var sensor = new Sensor<double?>(property.Name, DeviceClass.Precipitation, stateClass,
+                    isMetric ? "mm" : "in", isMetric ? I2M(value) : value);
+                result.Sensors.Add(sensor);
+            }
+            else if (property.Name.StartsWith("SoilMoisture"))
+            {
+                var sensor = new Sensor<int?>(property.Name, DeviceClass.Moisture, StateClass.Measurement, "%", (int?)property.GetValue(apiData));
+                result.Sensors.Add(sensor);
+            } else if (property.Name.StartsWith("SoilAd"))
+            {
+                var sensor = new Sensor<int?>(property.Name, DeviceClass.None, StateClass.Measurement, "", (int?)property.GetValue(apiData));
+                result.Sensors.Add(sensor);
+            } else if (property.Name.StartsWith("Pm25"))
+            {
+                var stateClass = property.Name.EndsWith("Avg24h") ? StateClass.Total : StateClass.Measurement;
+                
+                var sensor = new Sensor<double?>(property.Name, DeviceClass.Pm25, stateClass, "µg/m³", (double?)property.GetValue(apiData));
+                result.Sensors.Add(sensor); 
+            } else if (property.Name.StartsWith("Pm10"))
+            {
+                var stateClass = property.Name.EndsWith("Avg24h") ? StateClass.Total : StateClass.Measurement;
+                var sensor = new Sensor<double?>(property.Name, DeviceClass.Pm10, stateClass, "µg/m³", (double?)property.GetValue(apiData));
+                result.Sensors.Add(sensor);
+            } else if (property.Name.StartsWith("Pm1"))
+            {
+                var stateClass = property.Name.EndsWith("Avg24h") ? StateClass.Total : StateClass.Measurement;
+                var sensor = new Sensor<double?>(property.Name, DeviceClass.Pm1, stateClass, "µg/m³",
+                    (double?)property.GetValue(apiData));
+                result.Sensors.Add(sensor);
+            } else if (property.Name.StartsWith("Co2"))
+            {
+                var stateClass = property.Name.EndsWith("Avg24h") ? StateClass.Total : StateClass.Measurement;
+                var sensor = new Sensor<int?>(property.Name, DeviceClass.CarbonDioxide, stateClass, "ppm", (int?)property.GetValue(apiData));
+                result.Sensors.Add(sensor);
+            } else if (property.Name.StartsWith("LightningNum"))
+            {
+                var sensor = new Sensor<int?>(property.Name, DeviceClass.None, StateClass.Measurement, "", (int?)property.GetValue(apiData));
+                result.Sensors.Add(sensor);
+            } else if (property.Name.StartsWith("Lightning"))
+            {
+                var sensor = new Sensor<double?>(property.Name, DeviceClass.Distance, StateClass.Measurement,
+                    isMetric ? "km" : "miles",
+                    isMetric ? M2K((double?)property.GetValue(apiData)) : (double?)property.GetValue(apiData));
+                result.Sensors.Add(sensor);
+            } else if (property.Name.StartsWith("LightningTime"))
+            {
+                var sensor = new Sensor<string>(property.Name, DeviceClass.Timestamp, StateClass.Measurement, "", (string)property.GetValue(apiData));
+                result.Sensors.Add(sensor);
+            } else if (property.Name.StartsWith("Leak")) 
+            {
+                var sensor = new Sensor<int?>(property.Name, DeviceClass.None, StateClass.Measurement, "", (int?)property.GetValue(apiData));
+                result.Sensors.Add(sensor);
+            } else if (property.Name.StartsWith("Tf"))
+            {
+                var sensor = new Sensor<double?>(property.Name, DeviceClass.None, StateClass.Measurement, "",
+                    (double?)property.GetValue(apiData));
+            } else if (property.Name.StartsWith("LeafWetness"))
+            {
+                var sensor = new Sensor<int?>(property.Name, DeviceClass.Humidity, StateClass.Measurement, "%",
+                    (int?)property.GetValue(apiData));
+                result.Sensors.Add(sensor);
+            } else if (property.Name.Contains("Batt", StringComparison.InvariantCultureIgnoreCase))
+            {
+                var sensor = new Sensor<double?>(property.Name, DeviceClass.Battery, StateClass.Measurement, "%",
+                    (double?)property.GetValue(apiData));
+                result.Sensors.Add(sensor);
+            }
+        }
+
+        return result;
     }
     
     private static double? F2C(double? fahrenheit)
---
+++ src/Ecowitt.Controller/Model/Sensor.cs
@@ -0,0 +1,168 @@
+namespace Ecowitt.Controller.Model;
+
+public interface ISensor
+{
+    public string Name { get;  }
+    public DeviceClass DeviceClass { get;  }
+    public StateClass StateClass { get;  }
+    public string UnitOfMeasurement { get;  }
+    public object Value { get; }
+    public Type DataType { get; }
+}
+
+public interface ISensor<T> : ISensor
+{
+    new T Value { get; }
+}
+
+public class Sensor<T> : ISensor<T>
+{
+    private readonly T _value;
+
+    public Sensor(string name, DeviceClass deviceClass, StateClass stateClass, string unitOfMeasurement, T value)
+    {
+        Name = name;
+        DeviceClass = deviceClass;
+        StateClass = stateClass;
+        UnitOfMeasurement = unitOfMeasurement;
+        Value = value;
+    }
+
+    public string Name { get; }
+    public DeviceClass DeviceClass { get; }
+    public StateClass StateClass { get; }
+    public string UnitOfMeasurement { get; }
+    public T Value { get; }
+    
+    object ISensor.Value => Value;
+    
+    public Type DataType => typeof(T);
+}
+
+/// <summary>
+/// from: https://www.home-assistant.io/integrations/sensor/#device-class
+/// The following device classes are supported for sensors:
+///
+///    None: Generic sensor. This is the default and doesn’t need to be set.
+///    apparent_power: Apparent power in VA.
+///    aqi: Air Quality Index (unitless).
+///    atmospheric_pressure: Atmospheric pressure in cbar, bar, hPa, mmHg, inHg, kPa, mbar, Pa or psi
+///    battery: Percentage of battery that is left in %
+///    carbon_dioxide: Carbon Dioxide in CO2 (Smoke) in ppm
+///    carbon_monoxide: Carbon Monoxide in CO (Gas CNG/LPG) in ppm
+///    current: Current in A, mA
+///    data_rate: Data rate in bit/s, kbit/s, Mbit/s, Gbit/s, B/s, kB/s, MB/s, GB/s, KiB/s, MiB/s or GiB/s
+///    data_size: Data size in bit, kbit, Mbit, Gbit, B, kB, MB, GB, TB, PB, EB, ZB, YB, KiB, MiB, GiB, TiB, PiB, EiB, ZiB or YiB
+///    date: Date string (ISO 8601)
+///    distance: Generic distance in km, m, cm, mm, mi, yd, or in
+///    duration: Duration in d, h, min, or s
+///    energy: Energy in Wh, kWh, MWh, MJ, or GJ
+///    energy_storage: Stored energy in Wh, kWh, MWh, MJ, or GJ
+///    enum: Has a limited set of (non-numeric) states
+///    frequency: Frequency in Hz, kHz, MHz, or GHz
+///    gas: Gasvolume in m³, ft³ or CCF
+///    humidity: Percentage of humidity in the air in %
+///    illuminance: The current light level in lx
+///    irradiance: Irradiance in W/m² or BTU/(h⋅ft²)
+///    moisture: Percentage of water in a substance in %
+///    monetary: The monetary value (ISO 4217)
+///    nitrogen_dioxide: Concentration of Nitrogen Dioxide in µg/m³
+///    nitrogen_monoxide: Concentration of Nitrogen Monoxide in µg/m³
+///    nitrous_oxide: Concentration of Nitrous Oxide in µg/m³
+///    ozone: Concentration of Ozone in µg/m³
+///    ph: Potential hydrogen (pH) value of a water solution
+///    pm1: Concentration of particulate matter less than 1 micrometer in µg/m³
+///    pm25: Concentration of particulate matter less than 2.5 micrometers in µg/m³
+///    pm10: Concentration of particulate matter less than 10 micrometers in µg/m³
+///    power_factor: Power factor (unitless), unit may be None or %
+///    power: Power in W or kW
+///    precipitation: Accumulated precipitation in cm, in or mm
+///    precipitation_intensity: Precipitation intensity in in/d, in/h, mm/d or mm/h
+///    pressure: Pressure in Pa, kPa, hPa, bar, cbar, mbar, mmHg, inHg or psi
+///    reactive_power: Reactive power in var
+///    signal_strength: Signal strength in dB or dBm
+///    sound_pressure: Sound pressure in dB or dBA
+///    speed: Generic speed in ft/s, in/d, in/h, km/h, kn, m/s, mph or mm/d
+///    sulphur_dioxide: Concentration of sulphur dioxide in µg/m³
+///    temperature: Temperature in °C, °F or K
+///    timestamp: Datetime object or timestamp string (ISO 8601)
+///    volatile_organic_compounds: Concentration of volatile organic compounds in µg/m³
+///    volatile_organic_compounds_parts: Ratio of volatile organic compounds in ppm or ppb
+///    voltage: Voltage in V, mV
+///    volume: Generic volume in L, mL, gal, fl. oz., m³, ft³, or CCF
+///    volume_flow_rate: Volume flow rate in m³/h, ft³/min, L/min, gal/min
+///    volume_storage: Generic stored volume in L, mL, gal, fl. oz., m³, ft³, or CCF
+///    water: Water consumption in L, gal, m³, ft³, or CCF
+///    weight: Generic mass in kg, g, mg, µg, oz, lb, or st
+///    wind_speed: Wind speed in Beaufort, ft/s, km/h, kn, m/s, or mph
+/// </summary>
+public enum DeviceClass
+{
+    None,
+    ApparentPower,
+    Aqi,
+    AtmosphericPressure,
+    Battery,
+    CarbonDioxide,
+    CarbonMonoxide,
+    Current,
+    DataRate,
+    DataSize,
+    Date,
+    Distance,
+    Duration,
+    Energy,
+    EnergyStorage,
+    Enum,
+    Frequency,
+    Gas,
+    Humidity,
+    Illuminance,
+    Irradiance,
+    Moisture,
+    Monetary,
+    NitrogenDioxide,
+    NitrogenMonoxide,
+    NitrousOxide,
+    Ozone,
+    Ph,
+    Pm1,
+    Pm25,
+    Pm10,
+    PowerFactor,
+    Power,
+    Precipitation,
+    PrecipitationIntensity,
+    Pressure,
+    ReactivePower,
+    SignalStrength,
+    SoundPressure,
+    Speed,
+    SulphurDioxide,
+    Temperature,
+    Timestamp,
+    VolatileOrganicCompounds,
+    VolatileOrganicCompoundsParts,
+    Voltage,
+    Volume,
+    VolumeFlowRate,
+    VolumeStorage,
+    Water,
+    Weight,
+    WindSpeed
+}
+
+
+/// <summary>
+/// from: https://developers.home-assistant.io/blog/2021/09/20/state_class_total/
+/// There are 3 defined state classes:
+/// measurement: the state represents a measurement in present time, for example a temperature, electric power, etc. For supported sensors, statistics of min, max and average sensor readings are updated periodically.
+/// total: the state represents a total amount that can both increase and decrease, e.g. a net energy meter. When supported, the accumulated growth or decline of the sensor's value since it was first added is updated periodically.
+/// total_increasing: a monotonically increasing total, e.g. an amount of consumed gas, water or energy. When supported, the accumulated growth of the sensor's value since it was first added is updated periodically.
+/// </summary>
+public enum StateClass
+{
+    Measurement,
+    TotalIncreasing,
+    Total
+} 
