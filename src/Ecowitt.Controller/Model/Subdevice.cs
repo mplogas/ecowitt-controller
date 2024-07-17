1519ee02587635a4a9cc6785de38f82a85096819
mplogas <m.plogas@berlin.de>
2024-07-17 22:49:37 +0200

    subdevices also need to be refactored

--- src/Ecowitt.Controller/Model/Subdevice.cs
+++ src/Ecowitt.Controller/Model/Subdevice.cs
@@ -10,6 +10,9 @@ public class Subdevice
     public int Signal { get; set; }
     public string GwIp { get; set; }
     public DateTime TimestampUtc { get; set; }
+    
+    //TODO: rebuild payload to use a List of ISensor
+    public List<ISensor> Sensors { get; set; } = new List<ISensor>();
     public string Payload { get; set; } = string.Empty;
 }
 
