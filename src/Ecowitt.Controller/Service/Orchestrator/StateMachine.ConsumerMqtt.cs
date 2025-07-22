760bd5a395cb794fc893e8ba6411b273f841a0c6
Marc Plogas <marc.plogas@digitalnatives.de>
2025-07-22 23:11:26 +0200

    improved readability/maintainability via partial classes

---
+++ src/Ecowitt.Controller/Service/Orchestrator/StateMachine.ConsumerMqtt.cs
@@ -0,0 +1,81 @@
+﻿using Ecowitt.Controller.Message.Event;
+
+namespace Ecowitt.Controller.Service.Orchestrator
+{
+    public partial class StateMachine
+    {
+        public Task OnHandle(MqttServiceEvent message)
+        {
+            switch (message.EventType)
+            {
+                case MqttServiceEventType.Started:
+                    _logger.LogInformation("MQTT Service started");
+                    _lastServiceState = MqttServiceEventType.Started;
+                    //_mqttServiceStarted.TrySetResult(true);
+                    break;
+                case MqttServiceEventType.Stopped:
+                    _logger.LogWarning("MQTT Service stopped");
+                    _lastServiceState = MqttServiceEventType.Stopped;
+                    break;
+                case MqttServiceEventType.Error:
+                    _logger.LogError($"MQTT Service error: {message.Message}");
+                    _lastServiceState = MqttServiceEventType.Error;
+                    break;
+                case MqttServiceEventType.Heartbeat:
+                    _logger.LogDebug("MQTT Service heartbeat received");
+                    _lastServiceState = MqttServiceEventType.Heartbeat;
+                    break;
+                case MqttServiceEventType.Unknown:
+                default:
+                    _logger.LogWarning($"Unknown MQTT Service event: {message.EventType}");
+                    break;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task OnHandle(MqttConnectionEvent message)
+        {
+            switch (message.EventType)
+            {
+                case MqttConnectionEventType.Connected:
+                    _logger.LogInformation("MQTT Client connected");
+                    break;
+                case MqttConnectionEventType.Disconnected:
+                    _logger.LogWarning("MQTT Client disconnected");
+                    break;
+                case MqttConnectionEventType.Error:
+                    _logger.LogInformation($"MQTT Client error {message.Message}");
+                    break;
+                case MqttConnectionEventType.MessageReceived:
+                default:
+                    _logger.LogInformation($"MQTT Connection event received: {message.EventType}");
+                    break;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task OnHandle(HomeAssistantStatusEvent message)
+        {
+            switch (message.Status)
+            {
+                case HomeAssistantStatusType.Online:
+                    _logger.LogInformation("HomeAssistant is online");
+                    break;
+                case HomeAssistantStatusType.Offline:
+                    _logger.LogInformation("HomeAssistant is offline");
+                    break;
+                case HomeAssistantStatusType.Unknown:
+                default:
+                    _logger.LogInformation("Unknown HomeAssistant status received");
+                    break;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        
+
+    }
+}
