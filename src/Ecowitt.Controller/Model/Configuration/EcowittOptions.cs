7b213a73a869129f8501c814535689f57a9886c8
Marc Plogas <marc.plogas@digitalnatives.de>
2026-05-12 22:41:34 +0200

    feat: add IngestMode enum and LiveDataInterval config field

--- src/Ecowitt.Controller/Model/Configuration/EcowittOptions.cs
+++ src/Ecowitt.Controller/Model/Configuration/EcowittOptions.cs
@@ -3,6 +3,7 @@
 public class EcowittOptions
 {
     public int PollingInterval { get; set; } = 30;
+    public int LiveDataInterval { get; set; } = 5;
     public bool CalculateValues { get; set; } = true;
     public int Retries { get; set; } = 2;
     public List<GatewayOptions> Gateways { get; set; } = new();
@@ -15,4 +16,5 @@ public class GatewayOptions
     public string Password { get; set; } = string.Empty;
     public string Ip { get; set; } = string.Empty;
     public bool Subdevices { get; set; }
+    public IngestMode IngestMode { get; set; } = IngestMode.Push;
 }
---
+++ src/Ecowitt.Controller/Model/Configuration/IngestMode.cs
@@ -0,0 +1,7 @@
+namespace Ecowitt.Controller.Model.Configuration;
+
+public enum IngestMode
+{
+    Push = 0,
+    Poll = 1
+}
